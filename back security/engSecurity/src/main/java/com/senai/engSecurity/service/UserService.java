9a4ae669c6b6060827f0a778a1097eddd47e0ab0
lKauanF <142945263+lKauanF@users.noreply.github.com>
2024-11-19 18:11:54 -0300

    Add files via upload

---
+++ back security/engSecurity/src/main/java/com/senai/engSecurity/service/UserService.java
@@ -0,0 +1,30 @@
+package com.senai.engSecurity.service;
+
+import com.senai.engSecurity.model.User;
+import com.senai.engSecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsernameAndPassword(User user) {
+        return this.userRepository
+                .findByUsernameAndPassword(user.getUsername(), user.getPassword())
+                .orElseThrow(() -> new UsernameNotFoundException(user.getUsername()));
+    }
+}
